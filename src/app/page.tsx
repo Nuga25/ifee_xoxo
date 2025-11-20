f215d81830fc93b2ebde268a7570338701fd7109
Nuga25 <osinugaifeoluwa1@gmail.com>
2025-11-20 22:22:16 +0100

    adjusted button positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -208,7 +208,7 @@ export default function Home() {
 
             {/* CV button */}
 
-            <div className="fixed bottom-6 left-6 z-40">
+            <div className="fixed bottom-3 left-6 z-40">
               <button
                 onClick={handleDownload}
                 className="z-30 flex w-auto px-3 py-2 rounded-full border border-white/20 bg-white/1 backdrop-blur-sm text-white font-semibold transition-all duration-300 ease-in-out hover:shadow-[0_0_12px_#c779dd] hover:border-purple-400/50 active:shadow-[0_0_40px_#c779dd] active:scale-95 shadow-[inset_1px_1px_1px_rgba(255,255,255,0.8)]"
